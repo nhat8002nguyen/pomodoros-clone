8096e69f820bc449d81566d22cd359801fb1ee12
nhatnv <nv.nhat8002@gmail.com>
2021-04-25 16:00:51 +0700

    complete run and change time

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
-import { ContactSupportOutlined } from "@material-ui/icons";
 
 export default function Main() {
   const [timeType, setTimeType] = useState("Pomodoros");
-  const [timeRunning, setTimeRunning] = useState(1500);
-  const [pomoTime, setPomoTime] = useState(1500);
-  const [shortBreakTime, setShorBreakTime] = useState(300);
-  const [longBreakTime, setLongBreakTime] = useState(900);
-  const [countPomo, setCountPomo] = useState(1);
   const [runState, setRunState] = useState(false);
+  const [time, setTime] = useState(5);
+  const [timeRun, setTimeRun] = useState("");
+  const [numPomo, setNumPomo] = useState(1);
 
-  let startTime = runState
-    ? setInterval(() => {
-        setTimeRunning((prev) => prev - 1);
-      })
-    : null;
+  const onStartTime = () => {
+    setRunState(true);
+    triggerTime();
+  };
 
-  const onToggleTime = () => {
-    setRunState(!runState);
-    if (!runState) clearInterval(startTime);
+  let triggerTime = () =>
+    setTimeRun(
+      setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000)
+    );
+
+  useEffect(() => {
+    console.log(time);
+    // when time = -1 for a bit delay before move to next time type
+    if (time === -1) {
+      clearInterval(timeRun);
+      changeTimeType();
+    }
+  }, [time]);
+
+  useEffect(() => {
+    if (timeType === "Short Break") setTime(3);
+    else if (timeType === "Pomodoros") setTime(5);
+    else setTime(10);
+    setNumPomo((prev) => (prev === 4 ? 0 : prev + 1));
+    if (runState === true) triggerTime();
+    chageBodyBackgroundColor();
+  }, [timeType]);
+
+  const chageBodyBackgroundColor = () => {
+    document.body.style.backgroundColor =
+      (timeType === "Short Break" && "green") ||
+      (timeType === "Pomodoros" && "#f25b50") ||
+      (timeType === "Long Break" && "blue");
+  };
+
+  const changeTimeType = () => {
+    setTimeType(numPomo < 4 ? "Short Break" : "Long Break");
+    let newType;
+    if (timeType === "Pomodoros" && numPomo < 4) newType = "Short Break";
+    else if (timeType === "Pomodoros" && numPomo === 4) newType = "Long Break";
+    else newType = "Pomodoros";
+    setTimeType(newType);
+  };
+
+  const onStopTime = () => {
+    setRunState(false);
+    clearInterval(timeRun);
   };
 
   const secondsToMinutes = (seconds) => {
+    if (seconds < 0) return "00:00";
     const minutePart =
       Math.floor(seconds / 60) < 10
         ? "0" + Math.floor(seconds / 60)
@@ -35,23 +73,44 @@ export default function Main() {
     <div className="main-container">
       <div className="time-type-area">
         <div className="time-type-group">
-          <div className="time-type-box">
+          <div
+            className="time-type-box"
+            style={
+              timeType !== "Pomodoros" ? { backgroundColor: "#f06e65" } : null
+            }
+          >
             <text>Pomodoros</text>
           </div>
-          <div className="time-type-box">
+          <div
+            className="time-type-box"
+            style={
+              timeType !== "Short Break" ? { backgroundColor: "#f06e65" } : null
+            }
+          >
             <text>Short Break</text>
           </div>
-          <div className="time-type-box">
+          <div
+            className="time-type-box"
+            style={
+              timeType !== "Long Break" ? { backgroundColor: "#f06e65" } : null
+            }
+          >
             <text>Long Beak</text>
           </div>
         </div>
         <div className="time-run">
-          <text>{secondsToMinutes(timeRunning)}</text>
+          <text>{secondsToMinutes(time)}</text>
         </div>
         <div className="affect-time-group">
-          <div class="toggle-time-btn" onClick={onToggleTime}>
-            <text>{runState ? "Stop" : "Start"}</text>
-          </div>
+          {runState ? (
+            <div class="toggle-time-btn" onClick={onStopTime}>
+              <text>Stop</text>
+            </div>
+          ) : (
+            <div class="toggle-time-btn" onClick={onStartTime}>
+              <text>Start</text>
+            </div>
+          )}
           <div className="skip-next-btn">
             <SkipNextIcon fontSize="large" />
           </div>
